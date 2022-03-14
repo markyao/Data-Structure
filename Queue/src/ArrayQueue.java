59a06ae51103670a61c42e7752457825dbe84bf2
yaotailin <yaotailin@163.com>
2022-03-14 13:57:45 +0800

    数组队列

---
+++ Queue/src/ArrayQueue.java
@@ -0,0 +1,54 @@
+import java.util.StringJoiner;
+
+/**
+ * @author yaotailin
+ */
+public class ArrayQueue<E> implements Queue<E> {
+    private final Array<E> array;
+
+    public ArrayQueue(int capacity) {
+        array = new Array<>(capacity);
+    }
+
+    public ArrayQueue() {
+        array = new Array<>();
+    }
+
+    @Override
+    public void enqueue(E e) {
+        array.addLast(e);
+    }
+
+    @Override
+    public E dequeue() {
+        return array.removeFirst();
+    }
+
+    @Override
+    public E getFront() {
+        return array.getFirst();
+    }
+
+    @Override
+    public int getSize() {
+        return array.getSize();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return array.isEmpty();
+    }
+
+    public int getCapacity() {
+        return array.getCapacity();
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner joiner = new StringJoiner(", ", "front [ ", "] tail ");
+        for (int i = 0; i < array.getSize(); i++) {
+            joiner.add(array.get(i).toString());
+        }
+        return "Queue: " + joiner;
+    }
+}
