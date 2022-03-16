9f9030754834b6bf542aace8f21d991d804936be
yaotailin <yaotailin@163.com>
2022-03-16 22:46:15 +0800

    队列性能测试

--- Queue/src/ArrayQueue.java
+++ Queue/src/ArrayQueue.java
@@ -51,4 +51,16 @@ public class ArrayQueue<E> implements Queue<E> {
         }
         return "Queue: " + joiner;
     }
+
+    public static void main(String[] args) {
+        ArrayQueue<Integer> queue = new ArrayQueue<>();
+
+        for (int i = 0; i < 5; i++) {
+            queue.enqueue(i);
+            System.out.println(queue);
+        }
+
+        queue.dequeue();
+        System.out.println(queue);
+    }
 }
--- Queue/src/Main.java
+++ Queue/src/Main.java
@@ -1,13 +1,27 @@
+import java.util.Random;
+
 public class Main {
-    public static void main(String[] args) {
-        ArrayQueue<Integer> queue = new ArrayQueue<>();
 
-        for (int i = 0; i < 5; i++) {
-            queue.enqueue(i);
-            System.out.println(queue);
+    private static double testQueue(Queue<Integer> queue, int opCount) {
+        long startTime = System.nanoTime();
+        for (int i = 0; i < opCount; i++) {
+            queue.enqueue(new Random().nextInt(Integer.MAX_VALUE));
         }
+        for (int i = 0; i < opCount; i++) {
+            queue.dequeue();
+        }
+        long endTime = System.nanoTime();
 
-        queue.dequeue();
-        System.out.println(queue);
+        return (endTime - startTime) / 1000000000.0;
+    }
+
+    public static void main(String[] args) {
+        int opCount = 100000;
+        ArrayQueue<Integer> arrayQueue = new ArrayQueue<>();
+        double time1 = testQueue(arrayQueue, opCount);
+        System.out.println("ArrayQueue, time: " + time1 + " s");
+        LoopQueue<Integer> loopQueue = new LoopQueue<>();
+        double time2 = testQueue(loopQueue, opCount);
+        System.out.println("LoopQueue, time: " + time2 + " s");
     }
 }
