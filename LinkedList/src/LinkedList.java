aca109f7e7250004f69e20d15c01fe75b4c11ed8
yaotailin <yaotailin@163.com>
2022-03-18 13:44:38 +0800

    链表头增加元素

--- LinkedList/src/LinkedList.java
+++ LinkedList/src/LinkedList.java
@@ -25,4 +25,48 @@ public class LinkedList<E> {
             return e.toString();
         }
     }
+
+    private Node head;
+    private int size;
+
+    public LinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void addFirst(E e) {
+//        Node node = new Node(e);
+//        node.next = head;
+//        head =node;
+        head = new Node(e, head);
+        size++;
+    }
+
+    private void add(int index, E e) {
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("Illegal index.");
+        }
+        if (size == 0) {
+            addFirst(e);
+        } else {
+            Node pre = head;
+            for (int i = 0; i < index - 1; i++) {
+                pre = pre.next;
+            }
+//            Node node = new Node(e);
+//            node.next = pre.next;
+//            pre.next = node;
+
+            pre.next = new Node(e, pre.next);
+            size++;
+        }
+    }
 }
