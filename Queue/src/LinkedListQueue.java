769e696122da4b3a6a4ed9da00dad2d48abc0c90
yaotailin <yaotailin@163.com>
2022-03-24 14:00:33 +0800

    链表实现队列

---
+++ Queue/src/LinkedListQueue.java
@@ -0,0 +1,109 @@
+import java.util.StringJoiner;
+
+public class LinkedListQueue<E> implements Queue<E> {
+
+
+    private Node head, tail;
+    private int size;
+
+    public LinkedListQueue() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    @Override
+    public void enqueue(E e) {
+        if (tail == null) {
+            tail = new Node(e);
+            head = tail;
+        } else {
+            tail.next = new Node(e);
+            tail = tail.next;
+        }
+        size++;
+    }
+
+    @Override
+    public E dequeue() {
+        if (isEmpty()) {
+            throw new IllegalArgumentException("dequeue is empty.");
+        }
+        Node retNode = head;
+        head = head.next;
+        retNode.next = null;
+        if (head == null) {
+            tail = null;
+        }
+        size--;
+        return retNode.e;
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner joiner = new StringJoiner("->", "Queue: front ", "->NULL tail ");
+        Node cur = head;
+        while (cur != null) {
+            joiner.add(cur.toString());
+            cur = cur.next;
+        }
+        return joiner.toString();
+    }
+
+    public static void main(String[] args) {
+        LinkedListQueue<Integer> queue = new LinkedListQueue<>();
+
+        for (int i = 0; i < 10; i++) {
+            queue.enqueue(i);
+            System.out.println(queue);
+        }
+
+        queue.dequeue();
+        System.out.println(queue);
+        queue.dequeue();
+        System.out.println(queue);
+    }
+
+    @Override
+    public E getFront() {
+        if (isEmpty()) {
+            throw new IllegalArgumentException("dequeue is empty.");
+        }
+        return head.e;
+    }
+
+
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    private class Node {
+        public E e;
+        public Node next;
+
+        public Node(E e, Node next) {
+            this.e = e;
+            this.next = next;
+        }
+
+        public Node(E e) {
+            this(e, null);
+        }
+
+        public Node() {
+            this(null, null);
+        }
+
+        @Override
+        public String toString() {
+            return e.toString();
+        }
+    }
+}
