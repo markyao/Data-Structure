1284a39787b65263c2d17b3971b85806839efedd
yaotailin <yaotailin@163.com>
2022-03-29 22:19:00 +0800

    链表递归leetcode 203

--- leetcode203/src/ListNode.java
+++ leetcode203/src/ListNode.java
@@ -1,3 +1,5 @@
+import java.util.StringJoiner;
+
 public class ListNode {
     int val;
     ListNode next;
@@ -5,12 +7,30 @@ public class ListNode {
     ListNode() {
     }
 
+    ListNode(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            throw new IllegalArgumentException("array can not be empty");
+        }
+        val = arr[0];
+        ListNode cur = this;
+        for (int i = 1; i < arr.length; i++) {
+            cur.next = new ListNode(arr[i]);
+            cur = cur.next;
+        }
+    }
+
     ListNode(int val) {
         this.val = val;
     }
 
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
+    @Override
+    public String toString() {
+        StringJoiner joiner = new StringJoiner("->", "", "->NULL");
+        ListNode cur = this;
+        while (cur != null) {
+            joiner.add(String.valueOf(cur.val));
+            cur = cur.next;
+        }
+        return joiner.toString();
     }
 }
