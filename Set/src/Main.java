f1aa1c372061e03fb90f5b6e0bf8f33c5cb6734c
yaotailin <yaotailin@163.com>
2022-04-07 11:00:45 +0800

    链表和二分树复杂度

--- Set/src/Main.java
+++ Set/src/Main.java
@@ -1,35 +1,42 @@
 import java.util.ArrayList;
 
+/**
+ * @author yaotailin
+ */
 public class Main {
+
+    private static double testSet(Set<String> set, String filename) {
+
+        long startTime = System.nanoTime();
+
+        System.out.println(filename);
+        ArrayList<String> words = new ArrayList<>();
+        if (FileOperation.readFile(filename, words)) {
+            System.out.println("Total words: " + words.size());
+
+            for (String word : words) {
+                set.add(word);
+            }
+            System.out.println("Total different words: " + set.getSize());
+        }
+        long endTime = System.nanoTime();
+
+        return (endTime - startTime) / 1000000000.0;
+    }
 
     public static void main(String[] args) {
 
-        System.out.println("Pride and Prejudice");
+        String filename = "D:\\Lab\\Data-Structure\\Set\\src\\pride-and-prejudice.txt";
 
-        ArrayList<String> words1 = new ArrayList<>();
-        if (FileOperation.readFile("pride-and-prejudice.txt", words1)) {
-            System.out.println("Total words: " + words1.size());
-
-            BstSet<String> set1 = new BstSet<>();
-            for (String word : words1) {
-                set1.add(word);
-            }
-            System.out.println("Total different words: " + set1.getSize());
-        }
+        BstSet<String> bstSet = new BstSet<>();
+        double time1 = testSet(bstSet, filename);
+        System.out.println("BST Set: " + time1 + " s");
 
         System.out.println();
 
+        LinkedListSet<String> linkedListSet = new LinkedListSet<>();
+        double time2 = testSet(linkedListSet, filename);
+        System.out.println("Linked List Set: " + time2 + " s");
 
-        System.out.println("A Tale of Two Cities");
-
-        ArrayList<String> words2 = new ArrayList<>();
-        if (FileOperation.readFile("a-tale-of-two-cities.txt", words2)) {
-            System.out.println("Total words: " + words2.size());
-
-            BstSet<String> set2 = new BstSet<>();
-            for (String word : words2)
-                set2.add(word);
-            System.out.println("Total different words: " + set2.getSize());
-        }
     }
 }
