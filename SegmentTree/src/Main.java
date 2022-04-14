bbfc6d3a701481505eadd2a210d0a18d66c5e957
yaotailin <yaotailin@163.com>
2022-04-14 22:31:07 +0800

    线段树创建

---
+++ SegmentTree/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Integer[] nums = {-2, 0, 3, -5, 2, -1};
+
+        SegmentTree<Integer> tree = new SegmentTree<>(nums, ((a, b) -> a + b));
+        System.out.println(tree);
+    }
+}
--- SegmentTree/src/SegmentTree.java
+++ SegmentTree/src/SegmentTree.java
@@ -1,16 +1,36 @@
+import java.util.StringJoiner;
+
 /**
  * @author yaotailin
  */
 public class SegmentTree<E> {
     private E[] tree;
     private E[] data;
+    private Merger<E> merger;
 
-    public SegmentTree(E[] arr) {
+    public SegmentTree(E[] arr, Merger<E> merger) {
+        this.merger = merger;
         data = (E[]) new Object[arr.length];
         for (int i = 0; i < arr.length; i++) {
             data[i] = arr[i];
         }
         tree = (E[]) new Object[4 * arr.length];
+        buildTree(0, 0, data.length - 1);
+    }
+
+    private void buildTree(int treeIndex, int l, int r) {
+        if (l == r) {
+            tree[treeIndex] = data[l];
+            return;
+        }
+
+        int leftIndex = leftChild(treeIndex);
+        int rightIndex = rightChild(treeIndex);
+
+        int mid = l + (r - l) / 2;
+        buildTree(leftIndex, l, mid);
+        buildTree(rightIndex, mid + 1, r);
+        tree[treeIndex] = merger.merge(tree[leftIndex], tree[rightIndex]);
     }
 
     public int getSize() {
@@ -30,5 +50,20 @@ public class SegmentTree<E> {
 
     private int rightChild(int index) {
         return 2 * index + 2;
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner joiner = new StringJoiner(",", "[", "]");
+
+        for (E e : tree) {
+            if (e != null) {
+                joiner.add(e.toString());
+            } else {
+                joiner.add("null");
+            }
+        }
+
+        return joiner.toString();
     }
 }
