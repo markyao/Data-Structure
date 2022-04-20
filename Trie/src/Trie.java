0ef4c3755dd27eb61c84f217e5376fb14b8e94a8
yaotailin <yaotailin@163.com>
2022-04-20 13:46:44 +0800

    trie字典树基础

---
+++ Trie/src/Trie.java
@@ -0,0 +1,48 @@
+import java.util.TreeMap;
+
+/**
+ * @author yaotailin
+ */
+public class Trie {
+    private Node root;
+    private int size;
+
+    public Trie() {
+        root = new Node();
+        size = 0;
+    }
+
+    public void add(String word) {
+        Node cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (cur.next.get(c) == null) {
+                cur.next.put(c, new Node());
+            }
+            cur = cur.next.get(c);
+        }
+        if (!cur.isWord) {
+            cur.isWord = true;
+            size++;
+        }
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    private class Node {
+        public boolean isWord;
+        public TreeMap<Character, Node> next;
+
+        public Node(boolean isWord) {
+            this.isWord = isWord;
+            this.next = new TreeMap<>();
+        }
+
+        public Node() {
+            this(false);
+        }
+    }
+
+}
