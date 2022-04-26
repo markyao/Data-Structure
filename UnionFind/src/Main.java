4447582b0a4b06b61a13d19b756d125879f36ba8
yaotailin <yaotailin@163.com>
2022-04-26 14:56:41 +0800

    并查集路径压缩2及性能测试

--- UnionFind/src/Main.java
+++ UnionFind/src/Main.java
@@ -25,12 +25,19 @@ public class Main {
         int size = 10000000;
         int m = 10000000;
 //        UnionFind1 uf1 = new UnionFind1(size);
-//        System.out.println("UUnionFind1: " + TestUF(uf1, m) + " s");
+//        System.out.println("UnionFind1: " + TestUF(uf1, m) + " s");
 //        UnionFind2 uf2 = new UnionFind2(size);
-//        System.out.println("UUnionFind2: " + TestUF(uf2, m) + " s");
+//        System.out.println("UnionFind2: " + TestUF(uf2, m) + " s");
         UnionFind3 uf3 = new UnionFind3(size);
-        System.out.println("UUnionFind3: " + TestUF(uf3, m) + " s");
+        System.out.println("UnionFind3: " + TestUF(uf3, m) + " s");
+
         UnionFind4 uf4 = new UnionFind4(size);
-        System.out.println("UUnionFind4: " + TestUF(uf4, m) + " s");
+        System.out.println("UnionFind4: " + TestUF(uf4, m) + " s");
+
+        UnionFind5 uf5 = new UnionFind5(size);
+        System.out.println("UnionFind5: " + TestUF(uf5, m) + " s");
+
+        UnionFind6 uf6 = new UnionFind6(size);
+        System.out.println("UnionFind6: " + TestUF(uf6, m) + " s");
     }
 }
