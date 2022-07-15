856ad8f06d44a3862d08f85f9acae81806072cc0
yaotailin <yaotailin@163.com>
2022-07-15 21:08:30 +0800

    LeetCode1091 二进制矩阵中的最短路径

--- Graph/src/LeetCode1091.java
+++ Graph/src/LeetCode1091.java
@@ -16,6 +16,9 @@ public class LeetCode1091 {
         if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1) {
             return -1;
         }
+        if (n == 1) {
+            return 1;
+        }
         queue.add(0);
         visited[0][0] = true;
         while (!queue.isEmpty()) {
@@ -42,6 +45,43 @@ public class LeetCode1091 {
         return -1;
     }
 
+    public int shortestPathBinaryMatrixV2(int[][] grid) {
+        int[][] dirs = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}, {-1, -1}, {-1, 1}, {1, 1}, {1, -1}};
+        int n = grid.length;
+        boolean[][] visited = new boolean[n][n];
+        int[][] dist = new int[n][n];
+        Queue<Integer> queue = new LinkedList<>();
+
+        if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1) {
+            return -1;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        queue.add(0);
+        visited[0][0] = true;
+        dist[0][0] = 1;
+        while (!queue.isEmpty()) {
+            int path = queue.remove();
+            int x = path / n;
+            int y = path % n;
+            for (int d = 0; d < dirs.length; d++) {
+                int nx = x + dirs[d][0];
+                int ny = y + dirs[d][1];
+                if (nx >= 0 && nx < n && ny >= 0 && ny < n && grid[nx][ny] == 0 && !visited[nx][ny]) {
+                    int idx = nx * n + ny;
+                    queue.add(idx);
+                    visited[nx][ny] = true;
+                    dist[nx][ny] = dist[x][y] + 1;
+                    if (nx == n - 1 && ny == n - 1) {
+                        return dist[nx][ny];
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+
 
     public static void main(String[] args) {
         int[][] grid = {
@@ -50,6 +90,7 @@ public class LeetCode1091 {
 
         LeetCode1091 leetCode1091 = new LeetCode1091();
         System.out.println(leetCode1091.shortestPathBinaryMatrix(grid));
+        System.out.println(leetCode1091.shortestPathBinaryMatrixV2(grid));
 
     }
 }
